19526b80e58fbb0e1936f4f5d5d4d37ac4e75370
Cali Castle <hi@cali.so>
2022-12-06 15:53:19 +0800

    feat: add fonts and favicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Satoshi V', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-};
+}
