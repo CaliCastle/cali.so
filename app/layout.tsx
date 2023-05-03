86b68ac32707ca334fd23d2b89bbd198d210a954
Cali Castle <hi@cali.so>
2023-05-03 13:14:56 +0800

    chore: update body styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,7 +78,8 @@ export default function RootLayout({
       className={`${sansFont.variable} m-0 h-full p-0 font-sans antialiased`}
       suppressHydrationWarning
     >
-      <body className="flex h-full flex-col bg-[url('/grid-black.svg')] bg-top bg-repeat dark:bg-[url('/grid.svg')]">
+      <body className="flex h-full flex-col">
+        <div className="pointer-events-none fixed inset-0 select-none bg-[url('/grid-black.svg')] bg-top bg-repeat dark:bg-[url('/grid.svg')]" />
         <span className="pointer-events-none fixed top-0 block h-[800px] w-full select-none bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(5,5,5,0.045)_0%,rgba(0,0,0,0)_100%)] dark:bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(255,255,255,0.09)_0%,rgba(255,255,255,0)_100%)]" />
         <ThemeProvider
           attribute="class"
