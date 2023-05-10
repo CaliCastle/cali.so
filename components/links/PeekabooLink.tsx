e0e1ebadfd34262a701b09067103e44dae0719f0
Cali Castle <hi@cali.so>
2023-05-11 02:48:29 +0800

    fix: img component in PeekabooLink

--- components/links/PeekabooLink.tsx
+++ components/links/PeekabooLink.tsx
@@ -3,30 +3,10 @@
 import { clsxm } from '@zolplay/utils'
 import { AnimatePresence } from 'framer-motion'
 import { motion } from 'framer-motion'
-import Image from 'next/image'
 import Link, { type LinkProps } from 'next/link'
 import React from 'react'
 
 import { HoverCard } from '~/components/ui/HoverCard'
-
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="gshimmer">
-      <stop stop-color="#eee" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#eee" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#gshimmer)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite" />
-</svg>`
-
-const toBase64 = (str: string) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
 
 type PeekabooLinkProps = LinkProps &
   React.ComponentPropsWithoutRef<'a'> & {
@@ -99,16 +79,10 @@ export function PeekabooLink({
                   duration: 0.2,
                 }}
               >
-                <Image
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
                   src={`/api/link-preview?url=${href}`}
-                  placeholder="blur"
-                  blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                    shimmer(300, 150)
-                  )}`}
-                  priority
                   alt={`${href} 的预览图`}
-                  width={300}
-                  height={150}
                   className="pointer-events-none absolute left-0 top-0 h-full w-full rounded-xl object-cover"
                 />
               </motion.div>
