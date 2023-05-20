54a303bf4091fa3655edc3f41eeb8d012de6fbff
Cali Castle <hi@cali.so>
2023-05-20 16:23:18 +0800

    fix: remove liveblocks from env

--- sanity/env.ts
+++ sanity/env.ts
@@ -12,7 +12,7 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const useCdn = env.VERCEL_ENV === 'production'
+export const useCdn = env.NEXT_PUBLIC_SANITY_USE_CDN
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
