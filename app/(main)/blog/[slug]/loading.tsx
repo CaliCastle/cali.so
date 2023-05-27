45d16a8413ec971db31649f71c727d1d8a1a28ca
Cali Castle <hi@cali.so>
2023-05-27 23:43:02 +0800

    fix: optimize loader ufo color

--- app/(main)/blog/[slug]/loading.tsx
+++ app/(main)/blog/[slug]/loading.tsx
@@ -5,7 +5,7 @@ export default function BlogPostPageSkeleton() {
   return (
     <Container className="relative mt-16 min-h-screen lg:mt-32">
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="animate-pulse text-5xl">
+        <div className="animate-pulse text-5xl text-zinc-500/50">
           <UFOIcon />
         </div>
       </div>
