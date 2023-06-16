35ee8e0b58be036e2b68b85ee9f62764444986c2
Cali <hi@cali.so>
2023-06-16 20:40:59 +0800

    fix: post card border radius layer 2

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -37,7 +37,7 @@ export function BlogPostCard({ post, views }: { post: Post; views: number }) {
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
         />
       </div>
-      <span className="relative z-10 flex w-full flex-col gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-3xl before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-3xl after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
+      <span className="relative z-10 flex w-full flex-col gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
         <h2 className="z-20 text-base font-bold tracking-tight text-[--post-image-fg] opacity-70 transition-opacity group-hover:opacity-100 md:text-xl">
           {title}
         </h2>
