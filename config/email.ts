63fa48e2dde3dcc6418b800738440850680b301b
lemondy <lemondy9@gmail.com>
2024-01-29 23:45:04 +0800

    modify website name

--- config/email.ts
+++ config/email.ts
@@ -1,7 +1,7 @@
 export const emailConfig = {
-  from: 'hi@cali.so',
+  from: 'hi@lemondy.org',
   baseUrl:
     process.env.VERCEL_ENV === 'production'
-      ? `https://cali.so`
+      ? `https://lemondy.org`
       : 'http://localhost:3000',
 }
