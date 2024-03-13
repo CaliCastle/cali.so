f100037b931f66f7f9cec2f3f914e654abbe98fb
Cali Castle <hi@cali.so>
2024-03-13 22:55:51 +0800

    fix: hardcoded url method

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
+import { env } from '~/env.mjs'
+
 export function url(path = '') {
   const baseUrl =
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? env.NEXT_PUBLIC_SITE_URL
       : 'http://localhost:3000'
 
   return new URL(path, baseUrl)
